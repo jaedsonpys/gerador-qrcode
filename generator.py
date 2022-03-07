be424cd76fd99c4b97409f3d75ca9062321a1aa7
Jaedson Silva <contacanal798@gmail.com>
2022-03-07 09:19:55 -0300

    adicionando argumento "filename"

--- generator.py
+++ generator.py
@@ -2,16 +2,14 @@ import qrcode
 from datetime import datetime
 
 
-def make_qrcode(text: str) -> None:
-    now_time = datetime.now().strftime('%H-%M-%S')
-    qr_name = f'qr_code{now_time}.png'
+def make_qrcode(text: str, filename: str = None) -> None:
+    if not filename:
+        now_time = datetime.now().strftime('%H-%M-%S')
+        filename = f'qr_code{now_time}.png'
 
     qr = qrcode.QRCode(version=1, box_size=20, border=2)
     qr.add_data(text)
 
     img = qr.make_image()
-    img.save(qr_name)
-    return qr_name
-
-
-make_qrcode('texto')
+    img.save(filename)
+    return filename
